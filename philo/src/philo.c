9e6886336854afbed0510813fdefc31d285cc506
[Juan A.] <juan-aga@student.42.malaga.com>
2023-02-24 10:04:04 +0100

    ADD: ft_leaks MOD: .gitignore

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,15 +6,23 @@
 /*   By: juan-aga <juan_aga@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 09:03:13 by juan-aga          #+#    #+#             */
-/*   Updated: 2023/02/24 09:05:45 by juan-aga         ###   ########.fr       */
+/*   Updated: 2023/02/24 10:01:39 by juan-aga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdlib.h>
+
+static void	ft_leaks(void)
+{
+	system("leaks -q philo");
+}
 
 int	main(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
+
+	atexit(ft_leaks);
 	return (0);
 }
