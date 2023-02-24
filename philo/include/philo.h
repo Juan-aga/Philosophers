7c8a023397c9b932749f0b05a2acf20958d09db4
[Juan A.] <juan-aga@student.42.malaga.com>
2023-02-24 11:57:14 +0100

    ADD: struct t_phi.

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,12 +6,20 @@
 /*   By: juan-aga <juan_aga@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 09:04:48 by juan-aga          #+#    #+#             */
-/*   Updated: 2023/02/24 09:05:29 by juan-aga         ###   ########.fr       */
+/*   Updated: 2023/02/24 11:56:34 by juan-aga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+typedef struct s_phi
+{
+	int	num_phi;
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	num_te;
+}	t_phi;
 
 #endif
